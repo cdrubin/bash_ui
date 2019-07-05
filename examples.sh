aa3732e4f002142ba7d3cf078da21a23dc1534fd
cd rubin <daniel@rubin.za.net>
2019-07-05 18:33:59 -0400

    Fixed example again!

--- examples.sh
+++ examples.sh
@@ -4,16 +4,17 @@ source bash_ui.sh
 
 CHOICES=`find . -maxdepth 1 -mindepth 1 -type d -printf '%f\n' | sort`
 
-#choose_one
+choose_one
 echo "$CHOICE was chosen"
 echo
 
-#choose_multiple
+choose_multiple
 echo "$CHOSEN were chosen"
 echo "$CHOSEN_LINES lines were chosen"
 echo
 
 CHOICES=`cat hij.txt`
+
 choose_one_value
-echo "$CHOICE which corresponds to $CHOICE_VALUE was chosen"
+echo "$CHOICE which corresponds to value $CHOICE_VALUE was chosen"
 echo $VALUES
