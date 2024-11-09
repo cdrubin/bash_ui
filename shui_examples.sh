8d2c13a9584299f565baaee5e632a25fa976ad06
cd rubin <cefan@rubin.za.net>
2024-11-09 18:52:04 +0000

    shui properly disabling wordwrap now

--- shui_examples.sh
+++ shui_examples.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
-result=$(find . -maxdepth 1 -mindepth 1 -type d \( -not -iname '.*' \) -printf '%f\n' | ./shui 2>&1 1>/dev/tty)
+result=$(find . -maxdepth 1 -mindepth 1 -type d \( -not -iname '.*' \) -printf '%f\n' | ./shui clean 2>&1 1>/dev/tty)
 echo "Chosen directory: $result"
 
 result=$(find . -maxdepth 1 -mindepth 1 -type d \( -not -iname '.*' \) -printf '%f\n' | ./shui multiple 2>&1 1>/dev/tty)
 echo "Chosen directories: $result"
+
+
+read -r -d '' CHOICES <<- EOS
+a longer option here and in fact one that goes past the end of the line. Yes really these sorts of things have been known to happen!
+two
+three
+all
+EOS
+
+result=$(echo -e "$CHOICES" | ./shui 2>&1 1>/dev/tty)
